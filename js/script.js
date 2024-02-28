6751c7fab652061238d7923115b1586fca236010
generosotiago <tiagogeneroso47@gmail.com>
2024-02-28 20:29:38 -0300

    update script

--- js/script.js
+++ js/script.js
@@ -2,40 +2,33 @@ const steve = document.querySelector('.steve');
 const zombie = document.querySelector('.zombie');
 const background = document.querySelector('.background');
 
-const jump = () => {
-    steve.classList.add('jump');
+const jump = (event) => {
+    if (event.keyCode === 32) {
+        steve.classList.add('jump');
 
-    setTimeout(()=>{
+        setTimeout(() => {
+            steve.classList.remove('jump');
+        }, 900);
+    }
+};
 
-        steve.classList.remove('jump');
-   
-    }, 900);
-}
 const loop = setInterval(() => {
-
     const zombiePosition = zombie.offsetLeft;
-    const stevePosition = window.getComputedStyle(steve).bottom.replace('px','');
+    const stevePosition = parseInt(window.getComputedStyle(steve).getPropertyValue('bottom').replace('px', ''));
 
-    if (zombiePosition <= 50 && zombiePosition > 0 && stevePosition < 200){
-
+    if (zombiePosition <= 50 && zombiePosition > 0 && stevePosition < 200) {
         zombie.style.animation = 'none';
         zombie.style.left = `${zombiePosition}px`;
 
         steve.style.animation = 'none';
         steve.style.bottom = `${stevePosition}px`;
-        steve.src = 'images/gameover.png'
-        steve.style.width = '150px'
+        steve.src = 'images/gameover.png';
+        steve.style.width = '150px';
 
-        background.src = 'images/gameoverbkg.png'
-     
+        background.src = 'images/gameoverbkg.png';
+
         clearInterval(loop);
     }
-    
-   }
-
-, 10);
+}, 10);
 
 document.addEventListener('keydown', jump);
-
-console.log('kkkkkkkk')
-console.log('kkkkkkkk')console.log('kkkkkkkk')console.log('tiago')
